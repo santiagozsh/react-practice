ec101cdac2c327a33574177af425810019a060d9
Santiago Rodriguez Suareaz <sanrodsuz@pm.me>
2023-09-09 10:37:15 -0500

    third commit

--- 00-hola-mundo/src/main.jsx
+++ 00-hola-mundo/src/main.jsx
@@ -4,14 +4,20 @@ import ReactDOM from 'react-dom/client'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 const Button = ({text}) =>{
-
   return <button>{text}</button>
+}
+
+const Text = () =>{
+
+  return <h1>Hola Mundo</h1>
 
 }
 
 root.render(
 
   <React.Fragment>
+
+    <Text></Text>
 
     <Button text="Primer Boton"></Button>
     <Button text="Segundo Boton"></Button>
